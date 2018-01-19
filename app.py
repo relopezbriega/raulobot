10864b1d87044499b99cb194f7da6706c0aa7f56
Raul E. Lopez Briega <relopezbriega@gmail.com>
2018-01-18 23:38:17 -0300

    default response

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
 
-english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+raulobot = ChatBot("Chatterbot", 
+                   storage_adapter="chatterbot.storage.SQLStorageAdapter",
+                   logic_adapters=[
+        {
+            'import_path': 'chatterbot.logic.BestMatch'
+        },
+        {
+            'import_path': 'chatterbot.logic.LowConfidenceAdapter',
+            'threshold': 0.65,
+            'default_response': 'En mis blogs están todas las respuestas! https://relopezbriega.github.io/ - http://relopezbriega.com.ar/ - https://iaarbook.github.io/'
+        }
+    ],)
 
-english_bot.set_trainer(ChatterBotCorpusTrainer)
-#english_bot.train("chatterbot.corpus.english")
-english_bot.train("chatterbot.corpus.spanish")
+raulobot.set_trainer(ChatterBotCorpusTrainer)
+#raulobot.train("chatterbot.corpus.english")
+raulobot.train("chatterbot.corpus.spanish")
 
 
 @app.route("/")
@@ -18,7 +30,7 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(english_bot.get_response(userText))
+    return str(raulobot.get_response(userText))
 
 
 if __name__ == "__main__":
